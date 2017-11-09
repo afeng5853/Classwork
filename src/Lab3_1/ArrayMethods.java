63c8f5f1c5a722167bd02f64b6f6293dc4a7a6ef
BT_1N3_15 <BT_1N3_15@BT_1N3_15.bktech.local>
2017-11-09 13:16:12 -0500

    finished with comments

--- src/Lab3_1/ArrayMethods.java
+++ src/Lab3_1/ArrayMethods.java
@@ -6,12 +6,16 @@ import java.util.HashMap;
 import ljthw.CopyArrays;
 
 /**
- * 
+ * Lab 3.1
+ * <p> Contains useful array methods <p>
  * @author Alex Feng, Raymond Cheung
- *
+ * @since 11/9/2017
  */
+
 public class ArrayMethods {
+
 	public static void main(String[] args) {
+		/*
 		int[] test = {2, 3, 5, 6, 7, 7, 8, 8, 9};
 		int[] test2 = {2, 3, 5, 6, 7, 7, 8, 8, 9};
 		int[] testnodupe = removeDuplicates(test);
@@ -20,8 +24,15 @@ public class ArrayMethods {
 		//CopyArrays.print2DArray(productArr);
 		int[][] pascals = pascalTriangle(6);
 		printPascalTriangle(pascals);
+		*/
 	}
 	
+	/**
+	 * Returns a new array containing unique values of the original array.
+	 * @param  list	the array to be checked for duplicates
+	 * @return  	the array without duplicates
+	 */
+
 	public static int[] removeDuplicates(int[] list) {
 		boolean hasZero = false; // Since uninitialized vars in an int array becomes 0, we need to see if the array had a zero originally
 		int[] ansMedium = new int[list.length]; // container of duplicate values + null (0) if duplicates
@@ -60,9 +71,18 @@ public class ArrayMethods {
 
 		return ans;
 	}
+
+	/**
+	 * Creates a multiplication table using a multidimensional array containing the products of each combination of elements
+	 * @param arr1 array containing the multiplicands
+	 * @param arr2 array containing the multipliers
+	 * @return     array containing the products of the multiplicands and multipliers
+	 */
 	
 	public static int[][] productArray(int[] arr1, int[] arr2) {
+		// create a multidimensional array with row and column equal to arr1.length and arr2.length respectively
 		int[][] ans = new int[arr1.length][arr2.length];
+		
 		for (int i = 0; i < arr1.length; i++) {
 			for (int j = 0; j < arr2.length; j++) {
 				ans[i][j] = arr1[i] * arr2[j];
@@ -71,26 +91,32 @@ public class ArrayMethods {
 		return ans;
 	}
 	
-	private static int factorial(int n) {
-		int product = 1;
-		for (int i = 2; i <= n; i++) {
-			product *= i;
-		}
-		return product;
-	}
+	/**
+	 * Creates Pascal's triangle with specified number of rows
+	 * @param   n	the row of Pascal's triangle to be builded up to
+	 * @return	    a jagged array containing Pascal's triangle up to row n
+	 */
 	
 	public static int[][] pascalTriangle(int n) {
-		// formula : i!/(j!*(i-j)!)
 		int[][] ans = new int[n][];
 		for (int i = 0; i < n; i++) {
 			ans[i] = new int[i+1];
 			for (int j = 0; j <= i; j++) {
-				ans[i][j] = factorial(i) / (factorial(j) * factorial(i - j));
+				if (j == 0 || j == i) {
+					ans[i][j] = 1;
+				} else {
+					ans[i][j] = ans[i-1][j-1] + ans[i-1][j];
+				}
 			}
 		}
 		return ans;
-		
 	}
+	
+	/**
+	 * Prints Pascal's triangle
+	 * @param triangle pascals triangle as a jagged array
+	 */
+	
 	public static void printPascalTriangle(int[][] triangle) {
 		for (int i = 0; i < triangle.length; i++) {
 			int padding = triangle.length - i - 1;
