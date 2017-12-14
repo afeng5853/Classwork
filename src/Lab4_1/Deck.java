7a76895ac0fd2c2ddc65d3f506e77aebd40ec151
BT_1N3_15 <BT_1N3_15@BT_1N3_15.bktech.local>
2017-12-14 11:21:24 -0500

    add comments

--- src/Lab4_1/Deck.java
+++ src/Lab4_1/Deck.java
@@ -9,6 +9,7 @@ public class Deck {
 	
 	public Deck(String[] ranks, String[] suits, int[] points) {
 		ArrayList<Card> cards = new ArrayList<>();
+		// for each rank, create each suit for that rank
 		for (int i = 0; i < ranks.length; i++) {
 			for (int j = 0; j < suits.length; j++) {
 				cards.add(new Card(ranks[i], suits[j], points[i]));
@@ -17,24 +18,44 @@ public class Deck {
 		this.unDealt = cards;
 	}
 	
+	/**
+	 * returns whether the size of the Deck is 0
+	 * @return if the deck is empty
+	 */
 	public boolean isEmpty() {
 		return this.size() == 0;
 	}
 	
+	/**
+	 * returns the amount of the undealt cards
+	 * @return the remaining amount of cards in the deck
+	 */
 	public int size() {
 		return this.unDealt.size();
 	}
 	
+	/**
+	 * removes a card from the top of the deck by removing the last card and returning it
+	 * @return the card on top (the last card of the list)
+	 */
 	public Card deal() {
 		return this.unDealt.remove(this.size()-1);
 	}
 	
+	/**
+	 * exchanges two cards
+	 * @param i first card index
+	 * @param j second card index
+	 */
 	private void exchange(int i, int j) {
 		Card temp = unDealt.get(i);
 		unDealt.set(i, unDealt.get(j));
 		unDealt.set(j, temp);
 	}
 	
+	/**
+	 * shuffles the deck using an algorithm simialr to selection sort
+	 */
 	public void shuffle() {
 		Random r = new Random();
 		for (int k = 51; k > 0; k--) {
--- src/Lab4_1/DeckTester.java
+++ src/Lab4_1/DeckTester.java
@@ -1,13 +1,23 @@
 package Lab4_1;
-
+/**
+ * Lab 4.1
+ * @author Alex Feng
+ * @since 12/14/2017
+ */
 public class DeckTester {
 	public static void main(String[] args) {
+		// init deck fields
 		String[] ranks = new String[] {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
 		String[] suits = new String[] {"Diamond", "Club", "Spade", "Heart"};
 		int[] pointValues = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+		
+		// init deck
 		Deck deck = new Deck(ranks, suits, pointValues);
+		
 		deck.shuffle();
-		for (int i = 0; i < deck.size(); i++) {
+		
+		// print all of the cards in the deck
+		for (int i = deck.size(); i > 0; i--) {
 			System.out.println(deck.deal());
 		}
  	}
